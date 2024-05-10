6eee5f3defd629618c5219f1aeae4c8245d94607
james-kyrie915 <jameskyrie915@gmail.com>
2024-05-11 00:39:36 +0800

    day04_20_完成修改菜品功能

---
+++ src/main/java/com/blog/reggie/dto/DishDto.java
@@ -0,0 +1,18 @@
+package com.blog.reggie.dto;
+
+import com.blog.reggie.entity.Dish;
+import com.blog.reggie.entity.DishFlavor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class DishDto extends Dish {
+
+    private List<DishFlavor> flavors = new ArrayList<>();
+
+    private String categoryName;
+
+    private Integer copies;
+}
---
+++ src/main/java/com/blog/reggie/service/impl/DishFlavorServiceImpl.java
@@ -0,0 +1,13 @@
+package com.blog.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.blog.reggie.entity.DishFlavor;
+import com.blog.reggie.mapper.DishFlavorMapper;
+import com.blog.reggie.service.DishFlavorService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
+
+}
+
